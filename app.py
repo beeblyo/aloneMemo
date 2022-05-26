5902d4b216a35893f584016fe06db83bbf569ed6
beebly <qw1460@naver.com>
2022-05-26 17:42:03 +0900

    기사 저장과 보여주기 && DB저장

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
-import requests
-from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
+
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+
+
 
 ## HTML을 주는 부분
 @app.route('/')
@@ -15,16 +20,41 @@ def home():
 
 @app.route('/memo', methods=['GET'])
 def listing():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg':'GET 연결되었습니다!'})
+
+    memos = list(db.aloneMemo.find({},{'_id':False}))
+    # sample_receive = request.args.get('sample_give')
+    # print(sample_receive)
+    print(memos)
+    return jsonify({'memos':memos})
 
 ## API 역할을 하는 부분
 @app.route('/memo', methods=['POST'])
 def saving():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg':'POST 연결되었습니다!'})
+
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+
+    # url_receive = 'https://movie.naver.com/movie/bi/mi/basic.nhn?code=171539'
+    data = requests.get(url_receive,headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    title = soup.select_one("meta[property='og:title']")['content']
+    img = soup.select_one('meta[property="og:image"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
+    url = soup.select_one('meta[property="og:url"]')['content']
+    
+    #  DB SAVE
+    doc = {
+      'title':title,
+      'img':img,
+      'desc':desc,
+      'url':url,
+      'comment':comment_receive
+    }
+    db.aloneMemo.insert_one(doc)
+    
+    
+    return jsonify({'title':title, 'img':img, 'desc':desc, 'url':url})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
